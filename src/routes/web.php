544a3507d473be1e89b83c158adfb5c4f1244859
Darwin <salinash2000@gmail.com>
2018-05-15 10:55:11 -0600

    remove vendor
    remove vendor

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,12 +2,12 @@
 
 Route::get('manage/users', function () {
     return view('lpui::users');
-});
+})->middleware('auth');
 
 Route::get('manage/roles', function () {
     return view('lpui::roles');
-});
+})->middleware('auth');
 
 Route::get('manage/permissions', function () {
     return view('lpui::permissions');
-});
+})->middleware('auth');
